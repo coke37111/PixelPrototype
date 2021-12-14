bdb1e43a121fe96ad164fd4b52a63384885bb77b
minsu.kim <coke3711@naver.com>
2021-12-15 00:55:03 +0900

    샘플 코드 추가

--- GenSample/Assets/Scripts/PhotonSample/DummyManager.cs
+++ GenSample/Assets/Scripts/PhotonSample/DummyManager.cs
@@ -5,29 +5,23 @@ using UnityEngine;
 
 public class DummyManager : MonoBehaviour
 {
-    public GameObject _controlTarget;
-    public const float _moveSpeed = 100.0f;
+    public DummyPlayer _playerPrefab;
 
     private void Awake()
     {
     }
+
     void Start()
     {
+        var root = GetComponent<Canvas>().gameObject;
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            var obj = GameObject.Instantiate<GameObject>(_playerPrefab.gameObject, root.transform);
+            obj.GetComponent<DummyPlayer>().Player = player;
+        }
     }
         
     void Update()
     {
-
-        Vector3 delta = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-            delta.y += (_moveSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.S))
-            delta.y -= (_moveSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.A))
-            delta.x -= (_moveSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
-            delta.x += (_moveSpeed * Time.deltaTime);
-
-        _controlTarget.transform.position += delta;
     }
 }
