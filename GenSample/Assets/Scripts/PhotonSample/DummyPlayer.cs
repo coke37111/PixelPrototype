877cc2ea00d3558ec92b4229a55d330c058b32d7
minsu.kim <coke3711@naver.com>
2021-12-15 00:54:53 +0900

    샘플 코드 추가

---
+++ GenSample/Assets/Scripts/PhotonSample/DummyPlayer.cs
@@ -0,0 +1,38 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayer : MonoBehaviour
+{
+    public Player Player { get; set; }
+    private PhotonView _photonView;
+    public const float _moveSpeed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_photonView.AmOwner == false)
+            return;
+
+        Vector3 delta = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            delta.y += (_moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            delta.y -= (_moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            delta.x -= (_moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            delta.x += (_moveSpeed * Time.deltaTime);
+
+        transform.position += delta;
+    }
+
+}
