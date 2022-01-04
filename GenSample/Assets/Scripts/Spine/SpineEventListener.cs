3d42977d7f8ab3fe38f31ba13a82fb4fc3f17097
[4_개발] 김경섭 <megaman826@npixel.co.kr>
2022-01-04 15:44:59 +0900

    스파인 이벤트 로직 처리 구현

---
+++ GenSample/Assets/Scripts/Spine/SpineEventListener.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Util;
+using Spine.Unity;
+using UnityEngine;
+
+namespace Assets.Scripts.Spine
+{
+    [RequireComponent(typeof(SkeletonMecanim))]
+    public class SpineEventListener : MonoBehaviour
+    {
+        void testEvent_001()
+        {
+            Log.Print($"testEvent_001");
+        }
+    }
+}
