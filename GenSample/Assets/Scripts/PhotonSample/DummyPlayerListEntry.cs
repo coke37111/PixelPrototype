76a6cd076822836bfc0f9f90b53a7c6f2f9f1f1c
[4_UX] 정기영 <wjddjf80@gmail.com>
2022-02-11 16:40:42 +0900

    UI_TMP 적용

--- GenSample/Assets/Scripts/PhotonSample/DummyPlayerListEntry.cs
+++ GenSample/Assets/Scripts/PhotonSample/DummyPlayerListEntry.cs
@@ -90,7 +90,7 @@ namespace Photon.Pun.Demo.Asteroids
 
         public void SetPlayerReady(bool playerReady)
         {
-            PlayerReadyButton.GetComponentInChildren<Text>().text = playerReady ? "Ready!" : "Ready?";
+            PlayerReadyButton.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = playerReady ? "Ready!" : "Ready?";
             PlayerReadyImage.enabled = playerReady;
         }
     }
