20abf36d0c7868db66b69f3fcc275e19ac9dd9c3
[4_개발] 김경섭 <megaman826@npixel.co.kr>
2022-02-11 14:48:26 +0900

    DummyLobbyTopPanel 생성

---
+++ GenSample/Assets/Scripts/PhotonSample/DummyLobbyTopPanel.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.PhotonSample
+{
+    public class DummyLobbyTopPanel : MonoBehaviour
+    {
+
+        private readonly string connectionStatusMessage = "    Connection Status: ";
+
+        [Header("UI References")]
+        public TMPro.TextMeshProUGUI ConnectionStatusText;
+
+        #region UNITY
+
+        public void Update()
+        {
+            ConnectionStatusText.text = connectionStatusMessage + PhotonNetwork.NetworkClientState;
+        }
+
+        #endregion
+    }
+}
